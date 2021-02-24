1661f415a636f3bcdd81c562b090a3a13aca8a20
liy <1064012668@qq.com>
2021-02-24 17:50:38 +0800

    创建adapter

---
+++ src/main/java/web/handlerAdapter/RequestMappingHandlerAdapterStu.java
@@ -0,0 +1,13 @@
+package web.handlerAdapter;
+
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+/**
+ * {@linkplain RequestMappingHandlerAdapter}
+ *
+ * @author top.lioyan
+ * @version 1.0
+ * @date 2021/2/24 5:46 下午
+ */
+public class RequestMappingHandlerAdapterStu {
+}
